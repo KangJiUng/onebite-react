71e6781b394d6508d48606db2d2955f2a989697a
KangJiUng <woong236406@gmail.com>
2025-11-07 16:00:52 +0900

    chapter05

--- section03/src/index.js
+++ section03/src/index.js
@@ -3,7 +3,11 @@
 
 // ESM (ES 모듈 시스템)
 import mul, { add, sub } from "./math.js";
+import randomColor from "randomcolor";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(1, 2));
+const color = randomColor();
+console.log(color);
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(1, 2));
