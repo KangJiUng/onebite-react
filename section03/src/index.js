9ce7fe7798f54b184a25028f5f16b748b6a3f73b
KangJiUng <woong236406@gmail.com>
2025-11-07 15:49:25 +0900

    chapter04

--- section03/src/index.js
+++ section03/src/index.js
@@ -1,1 +1,9 @@
-console.log("안녕 Node.js");
+// CJS(Common JS 모듈 시스템)
+// const { add, sub } = require("./math.js");
+
+// ESM (ES 모듈 시스템)
+import mul, { add, sub } from "./math.js";
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(1, 2));
