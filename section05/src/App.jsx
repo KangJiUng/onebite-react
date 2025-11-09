8274ede752d7f2256c9fbe9d3ce8b01bb70c3895
KangJiUng <woong236406@gmail.com>
2025-11-09 16:51:02 +0900

    chapter07

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,35 +1,71 @@
 import "./App.css";
-import { useState } from "react";
+import Bulb from "./conponents/Bulb";
+import Counter from "./conponents/Counter";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
-  // let light = "OFF"; 로 일반 변수를 사용하면 리렌더링 되지 않음
-
   return (
     <>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === "ON" ? "OFF" : "ON");
-          }}
-        >
-          전구 {light === "ON" ? "끄기" : "켜기"}
-        </button>
-      </div>
-      <div>
-        <h1>{count}</h1>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
+      <Bulb />
+
+      <Counter />
     </>
   );
 }
 
 export default App;
+
+// ---------------------------------------
+// import "./App.css";
+// import { useState } from "react";
+
+// const Bulb = () => {
+//   const [light, setLight] = useState("OFF");
+
+//   return (
+//     <div>
+//       {light === "ON" ? (
+//         <h1 style={{ backgroundColor: "orange" }}>ON</h1>
+//       ) : (
+//         <h1 style={{ backgroundColor: "grey" }}>OFF</h1>
+//       )}
+//       <button
+//         onClick={() => {
+//           setLight(light === "ON" ? "OFF" : "ON");
+//         }}
+//       >
+//         전구 {light === "ON" ? "끄기" : "켜기"}
+//       </button>
+//     </div>
+//   );
+// };
+
+// const Counter = () => {
+//   const [count, setCount] = useState(0);
+
+//   return (
+//     <div>
+//       <h1>{count}</h1>
+//       <button
+//         onClick={() => {
+//           setCount(count + 1);
+//         }}
+//       >
+//         +
+//       </button>
+//     </div>
+//   );
+// };
+
+// function App() {
+//   const [light, setLight] = useState("OFF");
+//   // let light = "OFF"; 로 일반 변수를 사용하면 리렌더링 되지 않음
+
+//   return (
+//     <>
+//       <Bulb />
+//       <Counter />
+//     </>
+//   );
+// }
+
+// export default App;
