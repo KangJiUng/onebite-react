a63eb2d46c586a522d4fd5a39990473ef8aabc79
KangJiUng <woong236406@gmail.com>
2025-11-21 16:38:33 +0900

    chapter06-1

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -4,6 +4,11 @@ import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
 import Notfound from "./pages/NotFound";
+import emotion1 from "./assets/emotion1.png";
+import emotion2 from "./assets/emotion2.png";
+import emotion3 from "./assets/emotion3.png";
+import emotion4 from "./assets/emotion4.png";
+import emotion5 from "./assets/emotion5.png";
 
 // 1. "/": 모든 일기를 조회하는 Home 페이지
 // 2. "/new": 새로운 일기를 작성하는 New 페이지
@@ -17,6 +22,24 @@ function App() {
 
   return (
     <>
+      {/* 이미지를 public에서 불러옴 -> Vite의 이미지 최적화 진행 X */}
+      <div>
+        <img src={"/emotion1.png"} />
+        <img src={"/emotion2.png"} />
+        <img src={"/emotion3.png"} />
+        <img src={"/emotion4.png"} />
+        <img src={"/emotion5.png"} />
+      </div>
+
+      {/* 이미지를 assets에서 불러옴 -> Vite의 이미지 최적화 진행 O */}
+      {/* 소수의 이미지는 assets에 저장하는 것을 권장 */}
+      <div>
+        <img src={emotion1} />
+        <img src={emotion2} />
+        <img src={emotion3} />
+        <img src={emotion4} />
+        <img src={emotion5} />
+      </div>
       <div>
         <Link to={"/"}>Home</Link>
         <Link to={"/new"}>New</Link>
