e313b452a69275f08f4a19d80f7d2dea2601f3d3
KangJiUng <woong236406@gmail.com>
2025-11-21 19:06:23 +0900

    chapter11

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,16 +1,57 @@
+import { useState, useContext } from "react";
 import Header from "../components/Header";
 import Button from "../components/Button";
 import DiaryList from "../components/DiaryList";
+import { DiaryStateContext } from "../App";
+
+const getMontlyDate = (pivotDate, data) => {
+  // 달의 첫 날의 첫 시간
+  const beginTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth(),
+    1,
+    0,
+    0,
+    0
+  ).getTime();
+
+  // 달의 마지막 날의 마지막 시간
+  const endTime = new Date(
+    pivotDate.getFullYear(),
+    pivotDate.getMonth() + 1,
+    0, // 0일은 없으니까 해당 달의 이전 달의 마지막 날로 설정
+    23,
+    59,
+    59
+  );
+
+  return data.filter(
+    (item) => beginTime <= item.createdDate && item.createdDate <= endTime
+  );
+};
 
 const Home = () => {
+  const data = useContext(DiaryStateContext);
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const monthlyData = getMontlyDate(pivotDate, data);
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
   return (
     <div>
       <Header
-        title={"2024년 2월"}
-        leftChild={<Button text={"<"} />}
-        rightChild={<Button text={">"} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button onClick={onDecreaseMonth} text={"<"} />}
+        rightChild={<Button onClick={onIncreaseMonth} text={">"} />}
       />
-      <DiaryList />
+      <DiaryList data={monthlyData} />
     </div>
   );
 };
